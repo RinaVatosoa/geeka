3122cd44fe1e4b63b4f47d3766cbeec6020ff100
RinaVatosoa <rinavatosou@gmail.com>
2018-12-04 17:07:16 +0300

    externalisation et modification de l'arrière plan

--- src/app/Bundle/paralax/paralax.component.ts
+++ src/app/Bundle/paralax/paralax.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./paralax.component.scss']
 })
 export class ParalaxComponent implements OnInit {
+  title = "Our objectives";
+  introduction = "Community dedicated to women Developers for the sharing of experiences and skills in Computer Science."+
+      "\n" +
+      "We're sharing many interesting articles and themes every week.\n" +
+      "There will be also many topics for discussion.\n" +
+      "Then, big events will be organized with workshops and conferences to make our followers evolves";
+  greeting ="Welcome on geeka page!";
 
   constructor() { }
 
