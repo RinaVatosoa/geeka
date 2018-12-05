6e77aa5451a4b79f3e9aab05a38aa243ce5cc0a6
RinaVatosoa <rinavatosou@gmail.com>
2018-12-05 09:05:08 +0300

    Mise à jour du style

--- src/app/Component/coach/coach.component.ts
+++ src/app/Component/coach/coach.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./coach.component.scss']
 })
 export class CoachComponent implements OnInit {
+    opening = "They are our technical and professional guides. They are the ones who accompany " +
+        "us in the accomplishment and success of our projects.";
     role="Coach"
     isa:any={
         img:"./assets/img/isa.jpg",
